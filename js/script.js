15456ad460402bab96813dab361a005564acc8f8
ACE <acetip@aces-mbp.lab.lagcc>
2017-11-27 19:53:41 -0500

    creates dynamic search

--- js/script.js
+++ js/script.js
@@ -1,15 +1,31 @@
-fetch('https://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=dc6zaTOxFJmzC&limit=10')
-.then(function(response) {
-  return response.json();
-})
-.then(function(response) {
-  showGifs(response.data);
-});
+var input = document.querySelector('#user-gif-search');
+var searchBtn = document.querySelector('#submit-gif-search');
+var gifContainer = document.querySelector('#gif-result-container');
+
+function findGifs(userInput) {
+  fetch('https://api.giphy.com/v1/gifs/search?q=' + userInput + '&api_key=dc6zaTOxFJmzC&limit=15')
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(response) {
+    showGifs(response.data)
+  });
+} //Closes the findGifs function
 
 function showGifs(data) {
+  gifContainer.innerHTML = '';
   for (var x = 0; x < data.length; x++) {
     var img = document.createElement('img');
     img.src = data[x].images.original.url;
-    document.body.appendChild(img);
+    gifContainer.appendChild(img);
   }
+}
+
+if (input && searchBtn && gifContainer) {
+  searchBtn.addEventListener('click', function() {
+    var userInput = input.value;
+    if (userInput !== '') {
+      findGifs(userInput);
+    }
+  })
 }
